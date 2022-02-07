724c5ffeae1e3fe3f8f5a49c559df4307116b329
Cassia Taylor <cavwt2@cam.ac.uk>
2022-02-07 23:31:20 +0000

    First edits

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,17 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from .import datafetcher
+from .station import MonitoringStation
+(x,y) = (52.2053, 0.1218)
+
+def stations_by_distance(stations,p):
+   #define the tuple for p
+   p = (x,y)
+   for e in stations["items"]:
+       town = None
+       if "town" in e:
+           town = e["town"]
+        station_name = None
+        if "Station name" in e:
+            station_name = e["Station name"]
