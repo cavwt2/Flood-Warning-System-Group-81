0244c167a7c233e6fe20c76efc386529dc014e9e
Cassia Taylor <cavwt2@cam.ac.uk>
2022-02-08 14:34:34 +0000

    IB

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,17 +6,20 @@ geographical data.
 
 """
 
-from .utils import sorted_by_key  # noqa
-from haversine import haversine,unit
+from utils import sorted_by_key  # noqa
+from haversine import haversine
 (x,y) = (52.2053, 0.1218)
 
 def stations_by_distance(stations,p):
-   #define the tuple for p
+   """define the tuple for p"""
    p = (x,y)
+   """define empty list for the new list of tuples"""
    station_list = []
    for i in stations["items"]:
+       """creates a list of tuples each containing town name, station name and distance from p"""
        q = [i["town"],i["Station name"],haversine(i["coordinate"],p)]
        station_list.append(q)
+       """sorts the list by distance from p"""
        return sorted_by_key(q,2)
 
 
