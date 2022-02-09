9d301913f5cd17d47977a530d902bea37701d39d
JaeS <jaeheon.shim1@gmail.com>
2022-02-09 21:01:43 +0000

    Added test_all - just runs all codes to see if any errors pop up

---
+++ test_all.py
@@ -0,0 +1,14 @@
+import Task1A
+import Task1B
+import Task1C
+import Task1D
+import Task1E
+import Task1F
+
+def test_call():
+    Task1A.run()
+    Task1B.run()
+    Task1C.run()
+    Task1D.run()
+    Task1E.run()
+    Task1F.run()
