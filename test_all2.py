69fc9d0e780f067b67af319f566fa6adf95ea854
JaeS <jaeheon.shim1@gmail.com>
2022-03-01 18:20:21 +0000

    Fixed Task2E

---
+++ test_all2.py
@@ -0,0 +1,17 @@
+import Task2A
+import Task2B
+import Task2C
+import Task2D
+import Task2E
+import Task2F
+import Task2G
+
+
+Task2A.run()
+Task2B.run()
+Task2C.run()
+Task2D.run()
+Task2E.run()
+Task2F.run()
+Task2G.run()
+
